3483d145e7554f2762b1b4212c828a027e81c7ac
Leonardo Costa <43275999+leolpc21@users.noreply.github.com>
2022-10-10 15:56:26 -0300

    Update lancarPonto.spec.js

--- cypress/integration/MeuRH/lancarPonto.spec.js
+++ cypress/integration/MeuRH/lancarPonto.spec.js
@@ -5,11 +5,16 @@ import ponto from "../../support/Ponto";
 describe("Lançar horário de ponto no Meu RH", function () {
   const datas = [
     {
-      data: "10/08",
+      data: "10/10",
+    },
+  ];
+
+  const hrjus = [
+    {
       horaEntrada1: "07:00",
-      horaSaida1: "12:00",
-      horaEntrada2: "13:00",
-      horaSaida2: "16:30",
+      horaSaida1: "12:15",
+      horaEntrada2: "13:15",
+      horaSaida2: "17:15",
       justifEntrada1: "Entrada",
       justifSaida1: "Saída Almoço",
       justifEntrada2: "Entrada Almoço",
@@ -23,20 +28,22 @@ describe("Lançar horário de ponto no Meu RH", function () {
     ponto.menuEspelhoPonto();
   });
 
-  datas.forEach(function (dat) {
-    it("Lançar ponto referente a data " + dat.data, () => {
-      ponto.editarBatidaData(dat.data);
-      ponto.adicionarBatidas(
-        dat.horaEntrada1,
-        dat.horaSaida1,
-        dat.horaEntrada2,
-        dat.horaSaida2,
-        dat.justifEntrada1,
-        dat.justifSaida1,
-        dat.justifEntrada2,
-        dat.justifSaida2
-      );
-      ponto.botaoSalvar();
+  datas.forEach(function (data) {
+    hrjus.forEach(function (hrjus) {
+      it("Lançar ponto referente a data " + data.data, () => {
+        ponto.editarBatidaData(data.data);
+        ponto.adicionarBatidas(
+          hrjus.horaEntrada1,
+          hrjus.horaSaida1,
+          hrjus.horaEntrada2,
+          hrjus.horaSaida2,
+          hrjus.justifEntrada1,
+          hrjus.justifSaida1,
+          hrjus.justifEntrada2,
+          hrjus.justifSaida2
+        );
+        ponto.botaoSalvar();
+      });
     });
   });
 });
